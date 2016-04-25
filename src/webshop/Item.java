cb9ce8861600d60e11e489c327b4c3427d9c8363
alexm15 <alexander.markussen@live.dk>
2016-04-25 15:12:50 +0200

    Added information for Item class

--- src/webshop/Item.java
+++ src/webshop/Item.java
@@ -5,5 +5,19 @@ package webshop;
  * @author Niels
  */
 public class Item {
+    private int quantity;
+    private double sumPrice;
+
+    public Item(int quantity, double sumPrice)
+    {
+        this.quantity = quantity;
+        this.sumPrice = sumPrice;
+    }
+
+    public void changeQuantity(int quantity)
+    {
+        
+    }
     
+            
 }
