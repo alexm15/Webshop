7c969e925d61306a4cb762a4eefdccbefe37506b
Niels <Niels@NielsLaptop>
2016-04-25 14:59:17 +0200

    Added project

---
+++ src/webshop/Order.java
@@ -0,0 +1,15 @@
+package webshop;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Niels
+ */
+public class Order {
+    
+    private Date orderDate;
+    private double tax, shippingCharge, finalPrice;
+    private int orderID;
+    
+}
