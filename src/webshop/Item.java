bf3f578d3e69ac7fc6e55c9bb6f306d192179af6
Niels <Niels@NielsLaptop>
2016-04-25 15:17:09 +0200

    Oprettede Order og ændrede Item

--- src/webshop/Item.java
+++ src/webshop/Item.java
@@ -5,11 +5,14 @@ package webshop;
  * @author Niels
  */
 public class Item {
+    
+    private Product product;
     private int quantity;
     private double sumPrice;
 
-    public Item(int quantity, double sumPrice)
+    public Item(Product product, int quantity, double sumPrice)
     {
+        this.product = product;
         this.quantity = quantity;
         this.sumPrice = sumPrice;
     }
--- src/webshop/Order.java
+++ src/webshop/Order.java
@@ -1,6 +1,7 @@
 package webshop;
 
 import java.util.Date;
+import util.*;
 
 /**
  *
@@ -11,5 +12,33 @@ public class Order {
     private Date orderDate;
     private double tax, shippingCharge, finalPrice;
     private int orderID;
+    private Status status;
+    private Address shippingAddress;
+
+    public Order(Date orderDate, double tax, double shippingCharge, double finalPrice, int orderID, Status status, Address shippingAddress) {
+        this.orderDate = orderDate;
+        this.tax = tax;
+        this.shippingCharge = shippingCharge;
+        this.finalPrice = finalPrice;
+        this.orderID = orderID;
+        this.status = status;
+        this.shippingAddress = shippingAddress;
+    }
+    
+    public void addItem(Product product, int quantity) {
+        
+    }
+    
+    public void removeItem(Item item) {
+        
+    }
+    
+    public void changeQuantity(Item item, int quantity) {
+        
+    }
+    
+    public void returnItems() {
+        
+    }
     
 }
