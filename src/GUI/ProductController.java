71f1325226854fa12f0fcb8af7d7989129103063
Niels <Niels@NielsLaptop>
2016-04-25 16:18:27 +0200

    Tilføjede fejlbesked til login

--- src/GUI/ProductController.java
+++ src/GUI/ProductController.java
@@ -1,16 +1,13 @@
 package GUI;
 
 import java.net.URL;
-import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
 import webshop.Product;
 import webshop.WebshopDriver;
 
